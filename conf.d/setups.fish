f5e01f6771ab9df99b349e9796f8d99b91eb4adf
rafaScalet <114953004+rafaScalet@users.noreply.github.com>
2025-02-23 21:46:30 -0300

    change the cache directory and add util comands for ls

--- conf.d/setups.fish
+++ conf.d/setups.fish
@@ -1,11 +1,8 @@
 status is-interactive || exit
 
-set -g SETUPS_CACHE $HOME/.cache/setups
-mkdir -p $SETUPS_CACHE
-
 # Mise init setup
 if type -q ~/.local/bin/mise
-  set -l mise_init_cache "$SETUPS_CACHE/mise_fish_init.fish"
+  set -l mise_init_cache "$__fish_config_dir/conf.d/setups_mise_init.fish"
   if not test -f "$mise_init_cache"
     ~/.local/bin/mise activate fish > "$mise_init_cache"
   end
@@ -14,60 +11,66 @@ end
 
 # Mise completion setup
 if type -q usage
-  set -l mise_completions_cache "$SETUPS_CACHE/mise_fish_completions.fish"
+  set -l mise_completions_cache "$__fish_config_dir/conf.d/setups_mise_completions.fish"
   if not test -f "$mise_completions_cache"
     ~/.local/bin/mise completions fish > "$mise_completions_cache"
   end
-  source "$mise_completions_cache"
+
+  set -l usage_completions_cache "$__fish_config_dir/conf.d/setups_usage_completions.fish"
+  if not test -f "$usage_completions_cache"
+    usage --completions fish > "$usage_completions_cache"
+  end
 end
 
 # Bat-cat setup
 if type -q bat
-  set -l bat_completions_cache "$SETUPS_CACHE/bat_fish_completions.fish"
+  set -l bat_completions_cache "$__fish_config_dir/conf.d/setups_bat_completions.fish"
   if not test -f "$bat_completions_cache"
     bat --completion fish > "$bat_completions_cache"
   end
-  source "$bat_completions_cache"
   alias cat='bat'
-else if ype -q bat-cat
-  set -l bat_completions_cache "$SETUPS_CACHE/bat_fish_completions.fish"
+else if ype -q batcat
+  set -l bat_completions_cache "$__fish_config_dir/conf.d/setups_bat_completions.fish"
   if not test -f "$bat_completions_cache"
     bat-cat --completion fish > "$bat_completions_cache"
   end
-  source "$bat_completions_cache"
-  alias cat='bat-cat'
+  alias cat='batcat'
 end
 
 # Eza/Exa setup
 if type -q eza
   alias ls='eza --git --icons --color always --long --no-permissions --no-user --no-time --no-filesize'
+  alias lst='eza --git --icons --color always --long --no-permissions --no-user --no-time --no-filesize --tree'
   alias la='eza --git --icons --color always --long --all'
+  alias lat='eza --git --icons --color always --long --all --tree'
   alias ll='eza --git --icons --color always --long'
+  alias llt='eza --git --icons --color always --long --tree'
   alias lt='eza --icons --color always --tree'
 else if type -q exa
   alias ls='exa --git --icons --color always --long --no-permissions --no-user --no-time --no-filesize'
+  alias lst='exa --git --icons --color always --long --no-permissions --no-user --no-time --no-filesize --tre'
   alias la='exa --git --icons --color always --long --all'
+  alias lat='exa --git --icons --color always --long --all --tree'
   alias ll='exa --git --icons --color always --long'
+  alias llt='exa --git --icons --color always --long --tree'
   alias lt='exa --icons --color always --tree'
 end
 
 # Zoxide setup
 if type -q zoxide
-  set -l zoxide_cache "$SETUPS_CACHE/zoxide_fish_init.fish"
+  set -l zoxide_cache "$__fish_config_dir/conf.d/setups_zoxide_init.fish"
   if not test -f "$zoxide_cache"
     zoxide init fish --cmd cd > "$zoxide_cache"
   end
-  source "$zoxide_cache"
 end
 
 # TheFuck setup
 if type -q thefuck
-  set -l thefuck_cache "$SETUPS_CACHE/thefuck_fish_init.fish"
+  set -l thefuck_cache "$__fish_config_dir/conf.d/setups_thefuck_init.fish"
   if not test -f "$thefuck_cache"
     thefuck --alias > "$thefuck_cache"
     thefuck --alias fk > "$thefuck_cache"
   end
-  source "$thefuck_cache"
 end
 
 # Fastfetch setup
@@ -78,9 +81,8 @@ end
 
 # PNPM setup
 if type -q pnpm
-  set -l pnpm_cache "$SETUPS_CACHE/pnpm_fish_completion.fish"
+  set -l pnpm_cache "$__fish_config_dir/conf.d/setups_pnpm_completion.fish"
   if not test -f "$pnpm_cache"
     pnpm completion fish > "$pnpm_cache"
   end
-  source "$pnpm_cache"
 end
